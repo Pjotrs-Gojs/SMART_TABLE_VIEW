ccb7dd7bab073c30eeb3090b31d3f315b0b74662
Pjotrs Gojs <pjotrsgojs@gmail.com>
2019-10-22 10:08:05 +0000

    Coment controller,  add (App,pages,Page,content), Beautiful View

--- webapp/controller/MajorView.controller.js
+++ webapp/controller/MajorView.controller.js
@@ -9,31 +9,31 @@ sap.ui.define([
 		},
 		
 		select1: function () {
-			var oView = this.getView();
-			var vCategoryName = oView.byId("ST11").getSelectedItem().getBindingContext().getObject().CategoryName;
-			if (vCategoryName === null || vCategoryName === this.gCategoryName) {
-				oView.byId("ST11").removeSelections(true);
-			} else {
-				this.gCategoryName = this.getView().byId("ST11").getSelectedItem().getBindingContext().getObject().CategoryName;
-			}
+			// var oView = this.getView();
+			// var vCategoryName = oView.byId("ST11").getSelectedItem().getBindingContext().getObject().CategoryName;
+			// if (vCategoryName === null || vCategoryName === this.gCategoryName) {
+			// 	oView.byId("ST11").removeSelections(true);
+			// } else {
+			// 	this.gCategoryName = this.getView().byId("ST11").getSelectedItem().getBindingContext().getObject().CategoryName;
+			// }
 		},
 		select2: function () {
-			var oView = this.getView();
-			var vProductName = oView.byId("ST22").getSelectedItem().getBindingContext().getObject().ProductName;
-			if (vProductName === null || vProductName === this.gProductName) {
-				oView.byId("ST22").removeSelections(true);
-			} else {
-				this.gProductName = this.getView().byId("ST22").getSelectedItem().getBindingContext().getObject().ProductName;
-			}
+			// var oView = this.getView();
+			// var vProductName = oView.byId("ST22").getSelectedItem().getBindingContext().getObject().ProductName;
+			// if (vProductName === null || vProductName === this.gProductName) {
+			// 	oView.byId("ST22").removeSelections(true);
+			// } else {
+			// 	this.gProductName = this.getView().byId("ST22").getSelectedItem().getBindingContext().getObject().ProductName;
+			// }
 		},
 		select3: function () {
-			var oView = this.getView();
-			var vOrderID = oView.byId("ST33").getSelectedItem().getBindingContext().getObject().OrderID;
-			if (vOrderID === null || vOrderID === this.gOrderID) {
-				oView.byId("ST33").removeSelections(true);
-			} else {
-				this.gOrderID = this.getView().byId("ST33").getSelectedItem().getBindingContext().getObject().OrderID;
-			}
+			// var oView = this.getView();
+			// var vOrderID = oView.byId("ST33").getSelectedItem().getBindingContext().getObject().OrderID;
+			// if (vOrderID === null || vOrderID === this.gOrderID) {
+			// 	oView.byId("ST33").removeSelections(true);
+			// } else {
+			// 	this.gOrderID = this.getView().byId("ST33").getSelectedItem().getBindingContext().getObject().OrderID;
+			// }
 		}
 	});
 });
