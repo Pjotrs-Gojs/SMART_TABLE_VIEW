0cc92a3811d6d0b0fcde9a2d8041fe8bd974c46f
Pjotrs Gojs <pjotrsgojs@gmail.com>
2019-10-24 08:39:38 +0000

    Splitpane change left side properties, add filter on 2nd and 3th table, and visibility on table 2 and 3. Change 3 table on order_details

--- webapp/controller/MajorView.controller.js
+++ webapp/controller/MajorView.controller.js
@@ -1,30 +1,63 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/layout/SplitterLayoutData",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, SplitterLayoutData, Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("com.SMT.SMART_TABLE_VIEW.controller.MajorView", {
 		onInit: function () {
-
+			var oSplitterLayoutData2= new SplitterLayoutData({size: "0%"}),
+				oSplitterLayoutData3= new SplitterLayoutData({size: "0%"}),
+				oPane2= this.byId("T2"),
+				oPane3= this.byId("T3");
+	        	oPane2.setLayoutData(oSplitterLayoutData2);
+	        	oPane3.setLayoutData(oSplitterLayoutData3);
 		},
 		
-		select1: function () {
-			// var oView = this.getView();
-			// var vCategoryName = oView.byId("ST11").getSelectedItem().getBindingContext().getObject().CategoryName;
-			// if (vCategoryName === null || vCategoryName === this.gCategoryName) {
-			// 	oView.byId("ST11").removeSelections(true);
-			// } else {
-			// 	this.gCategoryName = this.getView().byId("ST11").getSelectedItem().getBindingContext().getObject().CategoryName;
-			// }
+		select1: function (oEvent) {
+			var oView = this.getView();
+			oView.byId("ST22").setVisible(true);
+			var oSplitterLayoutData2 = new SplitterLayoutData({size: "50%"}),
+				oPane2= this.byId("T2");
+	        	oPane2.setLayoutData(oSplitterLayoutData2);
+			var	vCategoryID = oView.byId("ST11").getSelectedItem().getBindingContext().getObject().CategoryID;
+	    	this._oTable = oView.byId("ST22");
+			var oBinding = this._oTable.getBinding("items"),
+				oFilter;
+				if (vCategoryID || vCategoryID === "") {
+				this._oTable.setShowOverlay(false);
+				oFilter = new sap.ui.model.Filter("CategoryID", "EQ", vCategoryID);
+				oBinding.filter([oFilter]);
+				}
+				if (vCategoryID === null || vCategoryID === this.gCategoryID) {
+					oView.byId("ST11").removeSelections(true);
+			} else {
+				this.gCategoryID = this.getView().byId("ST11").getSelectedItem().getBindingContext().getObject().CategoryID;
+				var oSplitterLayoutData3 = new SplitterLayoutData({size: "0%"}),
+				oPane3 = this.byId("T3");
+				oPane3.setLayoutData(oSplitterLayoutData3);
+			}
 		},
 		select2: function () {
-			// var oView = this.getView();
-			// var vProductName = oView.byId("ST22").getSelectedItem().getBindingContext().getObject().ProductName;
-			// if (vProductName === null || vProductName === this.gProductName) {
-			// 	oView.byId("ST22").removeSelections(true);
-			// } else {
-			// 	this.gProductName = this.getView().byId("ST22").getSelectedItem().getBindingContext().getObject().ProductName;
-			// }
+			var	oView = this.getView();
+			oView.byId("ST33").setVisible(true);
+			var oSplitterLayoutData2= new SplitterLayoutData({size: "33%"}),
+				oSplitterLayoutData3= new SplitterLayoutData({size: "33%"}),
+				oPane2= this.byId("T2"),
+				oPane3= this.byId("T3");
+	        	oPane2.setLayoutData(oSplitterLayoutData2);
+	        	oPane3.setLayoutData(oSplitterLayoutData3);
+	        var vProductID = oView.byId("ST22").getSelectedItem().getBindingContext().getObject().ProductID;
+	    	this._oTable1 = oView.byId("ST33");
+			var oBinding = this._oTable1.getBinding("items"),
+				oFilter;
+				if (vProductID || vProductID === "") {
+				this._oTable1.setShowOverlay(false);
+				oFilter = new sap.ui.model.Filter("ProductID", "EQ", vProductID);
+				oBinding.filter([oFilter]);
+				}
 		},
 		select3: function () {
 			// var oView = this.getView();
